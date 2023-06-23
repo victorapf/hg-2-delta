b2343b1aa6a8589cbd50253591ce527334c384f2
Victor Perez <81840736+victorapf@users.noreply.github.com>
2023-06-23 12:34:11 -0400

    Add files via upload

---
+++ Charlie/hack10.py
@@ -0,0 +1,4 @@
+lista =[100,200,300,400,500,600,700]
+lista.insert(5, "qux")
+lista.insert(6, "thud")
+print(lista)
---
+++ Charlie/hack9.py
@@ -0,0 +1,3 @@
+lista =[100,200,300,400,500,600,700]
+lista= lista.insert(0, "charlie")
+print(lista)
